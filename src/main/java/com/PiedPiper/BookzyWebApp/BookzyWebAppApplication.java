f3d30a152af6bfd2c466bdf94297a716859f0f06
student <jfbouopda@gmail.com>
2021-03-04 16:52:50 +0000

    first commit without pipeline file

---
+++ src/main/java/com/PiedPiper/BookzyWebApp/BookzyWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.PiedPiper.BookzyWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookzyWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookzyWebAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/PiedPiper/BookzyWebApp/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.PiedPiper.BookzyWebApp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BookzyWebAppApplication.class);
+	}
+
+}
